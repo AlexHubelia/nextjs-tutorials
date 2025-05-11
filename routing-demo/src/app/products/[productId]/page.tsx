73c0e8b08d748768c2c6823ed2a632ba342deb56
AlexHubelia <alexandre.gilbert@hubelia.com>
2025-05-11 13:22:22 -0400

    Metadata

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,8 +1,22 @@
-export default async function Product({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${id}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+}
+
+export default async function Product({ params }: Props) {
   const productId = (await params).productId;
   return <h1>Details about product {productId}</h1>;
 }
