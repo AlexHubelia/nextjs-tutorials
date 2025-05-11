0c817c20de1f5106a52c6392e5db39b61da470eb
AlexHubelia <alexandre.gilbert@hubelia.com>
2025-05-11 11:55:15 -0400

    Layouts

---
+++ layouts/src/app/(auth)/layout.tsx
@@ -0,0 +1,21 @@
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+        <footer
+          style={{
+            backgroundColor: "ghostwhite",
+            padding: "1rem",
+          }}
+        >
+          <p>Footer</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ layouts/src/app/(marketing)/layout.tsx
@@ -0,0 +1,29 @@
+export default function MarketingLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <header
+          style={{
+            backgroundColor: "lightblue",
+            padding: "1rem",
+          }}
+        >
+          <p>Header</p>
+        </header>
+        <main>{children}</main>
+        <footer
+          style={{
+            backgroundColor: "ghostwhite",
+            padding: "1rem",
+          }}
+        >
+          <p>Footer</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
